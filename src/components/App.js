ff904334b65e1b41ac6daab9853d13522478f3c0
mayank1503 <mayankmotwani1503@gmail.com>
2020-11-07 13:33:01 +0530

    Test

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,18 @@ import '../styles/App.css';
 
 class App extends Component {
     render() {
-
+        let relatives=["Mayank","Devesh","Girish"];
         return(
+           
             <div id="main">
                {/* Do not remove the main div */}
+            <ol key="relativeList">
+             {relatives.map((relative,index)=> (
+                 <li key={"relativeListItem" +(index+1)}>{relative}</li>
+             ))}
+            </ol>
             </div>
-        )
+        );
     }
 }
 
